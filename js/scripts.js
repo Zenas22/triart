02dacd561f47e82623aaf4c202287909d4d1359c
Zenas Muli <zenas2294@gmail.com>
2019-03-24 23:16:31 +0300

    Add innerHTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 var tri = [];
 function triart() {
 tri.push(parseInt(document.getElementById('l1').value));
-  if (isNaN(tri[0]) === true) {
+  if ( isNaN(tri[0]) ) {
     alert("Please enter a number in l1");
   }
   else {
   tri.push(parseInt(document.getElementById('l2').value));
-    if(isNaN(tri[1]) === true) {
+    if ( isNaN(tri[1]) ) {
       alert("Please enter a number in l2");
     }
     else {
     tri.push(parseInt(document.getElementById('l3').value));
-      if(isNaN(tri[2]) === true) {
+      if ( isNaN(tri[2]) ) {
         alert("Please enter a number in l3");
       }
       else {
@@ -20,15 +20,19 @@ tri.push(parseInt(document.getElementById('l1').value));
         }
         else if ( tri[0] === tri[1] && tri[1] === tri[2] && tri[2] === tri[0] ) {
           alert("Equilateral triangle confirmed");
+          document.getElementById('niambie').innerHTML = "<h2>This is an Equilateral triangle</h2> <p>This triangle has same lengths for all sides of the triangle. The sides intersect at angle of 60 degrees to give a total of 180 degrees</p>";
         }
         else if ( tri[0] === tri[1] || tri[1] === tri[2] || tri[2] === tri[0] ) {
           alert("Isosceles triangle confirmed");
+          document.getElementById('niambie').innerHTML = "<h2>This is an Isosceles triangle</h2> <p>This triangle has two sides with the same values for length but one side witha different length</p>";
         }
         else if ( (tri[0]+tri[1]) >= tri[2] || (tri[1]+tri[2]) >= tri[0] || (tri[2]+tri[0]) >= tri[1]){
           alert("Scalene triangle confirmed");
+          document.getElementById('niambie').innerHTML = "<h2>This is a Scalene triangle</h2> <p>The sides of this triangle all have different lengths and the may be further divided into Right angled triangles and obtuse triangles among others</p>";
         }
         else {
           alert("Please try again");
+          document.getElementById('niambie').innerHTML = "<h2>Please try again</h2>"
         }
         }
       }
