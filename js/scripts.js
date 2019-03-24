3ca634fe33ec5aa29ee2515f320dc14dacfb8a08
Zenas Muli <zenas2294@gmail.com>
2019-03-24 22:35:05 +0300

    Working logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,33 @@
-function triart(){
 var tri = [];
-tri.push(parseFloat(document.getElementById('l1').value));
-  if (isNan(tri[0]) === true){
+function triart() {
+tri.push(parseInt(document.getElementById('l1').value));
+  if (isNaN(tri[0]) === true) {
     alert("Please enter a number in l1");
-  }else{
-  tri.push(parseFloat(document.getElementById('l2').value));
-    if(isNan(tri[1]) === true){
+  }
+  else {
+  tri.push(parseInt(document.getElementById('l2').value));
+    if(isNaN(tri[1]) === true) {
       alert("Please enter a number in l2");
-    }else{
-    tri.push(parseFloat(document.getElementById('l3').value));
-      if(isNan(tri[2]) === true){
+    }
+    else {
+    tri.push(parseInt(document.getElementById('l3').value));
+      if(isNaN(tri[2]) === true) {
         alert("Please enter a number in l3");
-      }else{
-        if((tri[0]+tri[1])<=tri[2] || (tri[1]+tri[2])<=tri[0] || (tri[2]+tri[0])<=tri[1]){
+      }
+      else {
+        if ( (tri[0]+tri[1]) <= tri[2] || (tri[1]+tri[2]) <= tri[0] || (tri[2]+tri[0]) <= tri[1] ) {
           alert("Triangle not confirmed");
         }
-        else if (tri[0]===tri[1] && tri[1]===tri[2] && tri[2]===tri[0]){
+        else if ( tri[0] === tri[1] && tri[1] === tri[2] && tri[2] === tri[0] ) {
           alert("Equilateral triangle confirmed");
         }
-        else if (tri[0]===tri[1] || tri[1]===tri[2] || tri[2]===tri[0]){
+        else if ( tri[0] === tri[1] || tri[1] === tri[2] || tri[2] === tri[0] ) {
           alert("Isosceles triangle confirmed");
         }
-        else if ((tri[0]+tri[1])>=tri[2] || (tri[1]+tri[2])>=tri[0] || (tri[2]+tri[0])>=tri[1]){
+        else if ( (tri[0]+tri[1]) >= tri[2] || (tri[1]+tri[2]) >= tri[0] || (tri[2]+tri[0]) >= tri[1]){
           alert("Scalene triangle confirmed");
         }
-        else{
+        else {
           alert("Please try again");
         }
         }
