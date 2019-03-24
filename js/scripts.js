ceaae9d4c484be454a1967c12c0c8f0563357fee
Zenas Muli <zenas2294@gmail.com>
2019-03-24 22:01:35 +0300

    Change logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,16 @@
-function triart(){
+*document.getElementById('clickMe').onclick =
+function jaribu(){
 var tri = [];
-tri.push(parseFloat(document.getElementById('l1').value));
-  if (isNan(tri[0]) === true){
+tri.push(parseFloat(prompt("Enter l1: ")));
+  if (isNaN(tri[0]) === true){
     alert("Please enter a number in l1");
   }else{
-  tri.push(parseFloat(document.getElementById('l2').value));
-    if(isNan(tri[1]) === true){
+  tri.push(parseFloat(prompt("Enter l2: ")));
+    if(isNaN(tri[1]) === true){
       alert("Please enter a number in l2");
     }else{
-    tri.push(parseFloat(document.getElementById('l3').value));
-      if(isNan(tri[2]) === true){
+    tri.push(parseFloat(prompt("Enter l3: ")));
+      if(isNaN(tri[2]) === true){
         alert("Please enter a number in l3");
       }else{
         if((tri[0]+tri[1])<=tri[2] || (tri[1]+tri[2])<=tri[0] || (tri[2]+tri[0])<=tri[1]){
@@ -30,4 +31,4 @@ tri.push(parseFloat(document.getElementById('l1').value));
         }
       }
     }
-  }
+};
