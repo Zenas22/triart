2c53796f80acf04e392b0438b85b9bce18b02326
Zenas Muli <zenas2294@gmail.com>
2019-03-24 22:20:39 +0300

    Make logic work

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,20 @@
-*document.getElementById('clickMe').onclick =
-function jaribu(){
 var tri = [];
-tri.push(parseFloat(prompt("Enter l1: ")));
+function jaribu() {
+tri.push(parseInt(prompt("Enter l1:")));
   if (isNaN(tri[0]) === true){
     alert("Please enter a number in l1");
-  }else{
-  tri.push(parseFloat(prompt("Enter l2: ")));
+  }
+  else{
+  tri.push(parseInt(prompt("Enter l2:")));
     if(isNaN(tri[1]) === true){
       alert("Please enter a number in l2");
-    }else{
-    tri.push(parseFloat(prompt("Enter l3: ")));
+    }
+    else{
+    tri.push(parseInt(prompt("Enter l3:")));
       if(isNaN(tri[2]) === true){
         alert("Please enter a number in l3");
-      }else{
+      }
+      else{
         if((tri[0]+tri[1])<=tri[2] || (tri[1]+tri[2])<=tri[0] || (tri[2]+tri[0])<=tri[1]){
           alert("Triangle not confirmed");
         }
